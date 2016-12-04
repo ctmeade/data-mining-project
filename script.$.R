dc35d63332f1c1c9717e2b57125c52c318df8d68
ctmeade <meadectm@gmail.com>
2016-12-03 22:29:37 -0800

    4

--- script.$.R
+++ script.$.R
@@ -1,7 +1,6 @@
 #Libraries
 library(caret)
 library(mice)
-library(dplyr)
 
 #Import Data
 headers <- c("timesPregnant", "plasmaGlucose", "diastolicPressure", "tricepThickness", "serumInsulin", "bmi", "pedigreeFunction", "age", "diabetes")
@@ -41,7 +40,7 @@ md.pattern(data)
 #Can't drop that many observations
 #Must impute missing values
 
-#Multivariate Imputation by Chained Equations 
+#Multivariate Imputation by Predictive mean matching 
 
 tempdata <- mice(data, m = 1, method = 'pmm', seed = 131)
 data <- complete(tempdata, 1)
@@ -75,19 +74,25 @@ tenFoldCV <- trainControl(method = "repeatedcv",
 
 
 
-#Partition data in training and test sets
+###Partition data in training and test sets
 
-train <- sample.int(0.7*nrow(data), )
+sampleSize <- floor(.7 * nrow(data))
 
+set.seed(131)
+trainIndices <- sample(seq_len(nrow(data)), size = sampleSize)
 
+x.train <- data[trainIndices, 1:8]
+y.train <- data[trainIndices, 9]
 
-x.data <- data[,1:8]
-y.data <- as.factor(data[,9])
+x.test <- data[-trainIndices, 1:8]
+y.test <- data[-trainIndices, 9]
 
 #Models
 
-rf <- train(x.data,
-            y.data,
+#Random Forest
+set.seed(130)
+rf <- train(x.train,
+            y.train,
             method = "rf",
             metric = "ROC",
             trControl = tenFoldCV)
@@ -95,20 +100,25 @@ rf <- train(x.data,
 rf
 
 
-svm <- caret::train(x.data,
-                    y.data,
+#Support vector machine with radial bias kernel function
+set.seed(131)
+svm <- caret::train(x.train,
+                    y.train,
                     method = "svmRadial",
                     metric = "ROC",
                     trControl = tenFoldCV,
-                    tuneLength = 10)
+                    tuneLength = 6)
 
 svm
 
-svmExpandedGrid <- expand.grid(sigma = c(.07, .09, .11, .13, .15, .17, .19),
-                               C = c(.1, .15, .2, .25, .3, .35, .4))
+#Tune SVM models by expanding search grid around optimal sigma and C returned by the previous model
 
-svm2 <- caret::train(x.data,
-                     y.data,
+svmExpandedGrid <- expand.grid(sigma = c(.07, .1, .13),
+                               C = c(.4, .45, .5, .55, .6))
+
+set.seed(131)
+svm2 <- caret::train(x.train,
+                     y.train,
                      method = "svmRadial",
                      metric = "ROC",
                      trControl = tenFoldCV,
@@ -116,6 +126,8 @@ svm2 <- caret::train(x.data,
 
 svm2
 
+#Multi-layer perceptron neural network
+set.seed(131)
 mlpnn <- caret::train(x.data,
                       y.data,
                       method = "mlpML",
@@ -124,3 +136,20 @@ mlpnn <- caret::train(x.data,
 
 mlpnn
 
+#RF test accuracy
+
+rf.predict <- predict(rf$finalModel, x.test)
+rf.test.accuracy <- mean(rf.predict == y.test)
+rf.test.accuracy
+
+#SVM Test accuracy
+svm.predict <- predict(svm2$finalModel, x.test)
+svm.test.accuracy <- mean(svm.predict == y.test)
+svm.test.accuracy
+
+#MLPNN Test Accuracy
+mlpnn.predict <- predict(mlpnn$finalModel, x.test)
+mlpnn.predict <- as.data.frame(mlpnn.predict)
+mlpnn.predict$prediction <- ifelse(mlpnn.predict$V1 >= .5, "Diabetic", "notDiabetic")
+mlpnn.test.accuracy <- mean(mlpnn.predict$prediction == y.test)
+mlpnn.test.accuracy
