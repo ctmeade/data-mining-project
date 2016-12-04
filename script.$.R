81357b6924dc0418fb839b0ae9fdf0e69a92154d
ctmeade <meadectm@gmail.com>
2016-12-03 21:30:35 -0800

    3

--- script.$.R
+++ script.$.R
@@ -1,6 +1,7 @@
-#Librarys
-library(data.table)
+#Libraries
 library(caret)
+library(mice)
+library(dplyr)
 
 #Import Data
 headers <- c("timesPregnant", "plasmaGlucose", "diastolicPressure", "tricepThickness", "serumInsulin", "bmi", "pedigreeFunction", "age", "diabetes")
@@ -8,28 +9,62 @@ headers <- c("timesPregnant", "plasmaGlucose", "diastolicPressure", "tricepThick
 data <- read.csv(url("http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"),
                  header = FALSE,
                  col.names = headers)
-data$diabetes <- as.factor(ifelse(data$diabetes == 0, "notDiabetic", "Diabetic"))
-#Exploratory Analysis
 
+#Make the coded classification column more readable
+data$diabetes <- as.factor(ifelse(data$diabetes == 0, "notDiabetic", "Diabetic"))
+
+#####EXPLORATION
 
 ###Dealing with missing values
-#Data set not encoded with NA's
-#But does have 0's where biologocially impossible
-#Varibles include plasamaGlucose, diastolicPressure, tricepThickness, serumInsulin, bmi
-#Only 768 observation, so can't drop observations with missing valuessa
 
-#Take mean of nonmissing values in each column
-means <- sapply(data[,1:8], function(x) mean(x[x>0]))
+#Data set is not encoded with NA's
+#But there are 0's where biologocially impossible
+#This must be how NAs were encoded
+#Varibles with missing data include plasamaGlucose, diastolicPressure, tricepThickness, serumInsulin, bmi
 
-#Replace missing values with mean
-processedVariables <- c("plasamaGlucose", "diastolicPressure", "tricepThickness", "serumInsulin", "bmi")
+#Replace missing values with NA
+
+for (i in 2:6){
+  for (n in 1:nrow(data)){
+    if (data[n, i] == 0){
+      data[n, i] <- NA
+    }
+  }
+}
+
+#How many observations have missing values?
+
+md.pattern(data)
+
+#Only 392 complete observations
+#That means almost half of ovservations missing
+#Can't drop that many observations
+#Must impute missing values
+
+#Multivariate Imputation by Chained Equations 
+
+tempdata <- mice(data, m = 1, method = 'pmm', seed = 131)
+data <- complete(tempdata, 1)
 
 
+densityplot(tempdata)
 
-#Feature Selection
+#Look at distrubution of imputed data (magenta) vs original (blue)
+#Similar, so we're ok
+
+###Feature Selection
+
+#Need to make sure variablbes aren't correlated
 correlationMatrix <- cor(data[,1:8])
-highCorrelation <- findCorrelation(correlationMatrix, cutoff=0.4)
+
+#Cutoff at .7
+highCorrelation <- findCorrelation(correlationMatrix, cutoff=0.7)
 print(highCorrelation)
+#Dont need to eliminate variable
+
+#Standardization
+#Center and Scale data
+data[, 1:8] <- scale(data[, 1:8], center = TRUE, scale = TRUE)
 
 #Construct 10fold CV method
 tenFoldCV <- trainControl(method = "repeatedcv",
@@ -38,31 +73,54 @@ tenFoldCV <- trainControl(method = "repeatedcv",
                           classProbs=TRUE,
                           summaryFunction=twoClassSummary)
 
-#Standardization
+
+
+#Partition data in training and test sets
+
+train <- sample.int(0.7*nrow(data), )
+
+
+
 x.data <- data[,1:8]
 y.data <- as.factor(data[,9])
 
-#NB b/c of assumption of independence
-controlModel <- train(x.data,
-                      y.data,
-                      method = "rf", 
-                      preProcess = c("center","scale"), 
-                      trControl= tenFoldCV)
+#Models
 
-plot(controlModel, type=c("g", "o"))
+rf <- train(x.data,
+            y.data,
+            method = "rf",
+            metric = "ROC",
+            trControl = tenFoldCV)
 
-svm <- train(x.data,
-             y.data,
-             method = "svmLinear",
-             preProcess = c("center", "scale"),
-             trControl = tenFoldCV)
+rf
+
+
+svm <- caret::train(x.data,
+                    y.data,
+                    method = "svmRadial",
+                    metric = "ROC",
+                    trControl = tenFoldCV,
+                    tuneLength = 10)
 
 svm
 
-svm.tune <- train(x.data,
-                  y.data,
-                  method = "svmRadial",   # Radial kernel
-                  tuneLength = 2,					# 8 values of the cost function
-                  preProc = c("center","scale"),  # Center and scale data
-                  metric="ROC",
-                  trControl=tenFoldCV)
+svmExpandedGrid <- expand.grid(sigma = c(.07, .09, .11, .13, .15, .17, .19),
+                               C = c(.1, .15, .2, .25, .3, .35, .4))
+
+svm2 <- caret::train(x.data,
+                     y.data,
+                     method = "svmRadial",
+                     metric = "ROC",
+                     trControl = tenFoldCV,
+                     tuneGrid = svmExpandedGrid)
+
+svm2
+
+mlpnn <- caret::train(x.data,
+                      y.data,
+                      method = "mlpML",
+                      metric= "ROC",
+                      trControl=tenFoldCV)
+
+mlpnn
+
